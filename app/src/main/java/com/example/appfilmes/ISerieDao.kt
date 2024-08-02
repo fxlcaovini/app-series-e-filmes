d2d93c89cdc3a58f2036f7bdf036aa29b8adcfe9
falcaodevlearning <vinnehp@hotmail.com>
2024-08-01 22:41:29 -0300

    atualizando os parametros

--- app/src/main/java/com/example/appfilmes/ISerieDao.kt
+++ app/src/main/java/com/example/appfilmes/ISerieDao.kt
@@ -1,7 +1,8 @@
 package com.example.appfilmes
 
 import androidx.room.Dao
-import androidx.room.Delete import androidx.room.Insert
+import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
